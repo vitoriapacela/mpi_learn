38abe411cd545cdc39354f0c64ef4821188199f1
Vitoria Pacela <vbarinpa@rusty2.cm.cluster>
2018-07-18 04:32:28 -0400

    env setup in simons

---
+++ env/README.md
@@ -0,0 +1,5 @@
+# Environment for Simons Foundation's GPUs
+
+In the first time, execute `source setup_cuda9.sh`.
+
+To set up the environment for an interactive session after login in rusty, execute `source sf_env.sh`.
---
+++ env/train_mpi.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=mpi_learn
+#SBATCH -c 2
+#SBATCH --gres=gpu:1
+#SBATCH -p gpu
+#SBATCH -t 7-0
+#SBATCH --output train.out
+
+#!/bin/bash 
+
+## This script allocates any GPU!
+
+# Load necessary modules
+module purge
+
+module load slurm
+module load gcc
+module load python3
+module load cuda/9.0.176 
+module load cudnn/v7.0-cuda-9.0
+module load openmpi
+
+# Activate the virtual environment
+source ~/tf9/bin/activate
+ 
+#Executable here
+mpirun -n 3 python3 ../MPIGDriver.py ../simple_train_rmsprop_.json ../train_3d_energy.list ../test_3d_energy.list --loss categorical_crossentropy --epochs 2 --tf --features-name "image" --features-name X --labels-name y
+
